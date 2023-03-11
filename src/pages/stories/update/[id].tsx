a01253f82f61b2fa5971af89fe4f7658b348b517
kokily <kokily@naver.com>
2023-03-12 08:33:36 +0900

    Update Story page

---
+++ src/pages/stories/update/[id].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import AddStory from '@/components/stories/AddStory';
+
+const UpdateStoryPage: NextPage = () => {
+  return <AddStory />;
+};
+
+export default UpdateStoryPage;
