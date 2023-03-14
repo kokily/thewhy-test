9d7b8f5fcc7527761140cac254940ca5990df0f0
kokily <kokily@naver.com>
2023-03-14 12:02:34 +0900

    Old Data restore

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,64 @@
-import { Question } from '.prisma/client';
+import type { Question, Story } from '.prisma/client';
 import { PrismaClient } from '@prisma/client';
-import prevData from './question.json';
+import bcrypt from 'bcryptjs';
+import storiesData from './Story.json';
+import questionsData from './question.json';
 
 const db = new PrismaClient();
 
 async function seed() {
-  function getData() {
-    const data: Question[] = prevData.map((data) => {
+  // Admin Create
+  await db.admin.create({
+    data: {
+      password: await bcrypt.hash(process.env.PASSWORD, 10),
+    },
+  });
+
+  // Stories Restore
+  function getStories() {
+    const data: Story[] = storiesData.map((story) => {
       return {
-        id: data.id,
-        username: data.name,
-        password: data.password,
-        title: data.title,
-        body: data.body,
-        phone: data.phone,
-        email: data.email,
-        reply: data.reply,
-        createdAt: new Date(data.created_at),
-        updatedAt: new Date(data.updated_at),
+        id: story.id,
+        title: story.title,
+        body: story.body,
+        tags: story.tags,
+        thumbnail: story.thumbnail,
+        createdAt: new Date(story.createdAt),
+        updatedAt: new Date(story.updatedAt),
       };
     });
 
     return data;
   }
 
-  await Promise.all(
-    getData().map((data) => {
-      return db.question.create({ data });
-    })
-  );
+  // Questions Restore
+  function getQuestions() {
+    const data: Question[] = questionsData.map((question) => {
+      return {
+        id: question.id,
+        username: question.username,
+        password: question.password,
+        title: question.title,
+        body: question.body,
+        phone: question.phone,
+        email: question.email,
+        reply: question.reply,
+        createdAt: new Date(question.createdAt),
+        updatedAt: new Date(question.updatedAt),
+      };
+    });
+
+    return data;
+  }
+
+  await Promise.all([
+    getStories().map((story) => {
+      return db.story.create({ data: story });
+    }),
+    getQuestions().map((question) => {
+      return db.question.create({ data: question });
+    }),
+  ]);
 }
 
 seed();
