461aad212475c773c2db3b37233c5aaeb7629fbf
kokily <kokily@naver.com>
2023-03-19 09:16:22 +0900

    Question seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,34 @@
-import type { Question } from '.prisma/client';
+import type { Question } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
 import questionsData from './question.json';
 
 const db = new PrismaClient();
 
+function getQuestions() {
+  const data: Question[] = questionsData.map((question) => {
+    return {
+      id: question.id,
+      username: question.username,
+      password: question.password,
+      title: question.title,
+      body: question.body,
+      phone: question.phone,
+      email: question.email,
+      reply: question.reply,
+      createdAt: new Date(question.createdAt),
+      updatedAt: new Date(question.updatedAt),
+    };
+  });
+
+  return data;
+}
+
 async function seed() {
-  function getQuestions() {
-    const data: Question[] = questionsData.map((question) => {
-      return {
-        id: question.id,
-        username: question.username,
-        password: question.password,
-        title: question.title,
-        body: question.body,
-        phone: question.phone,
-        email: question.email,
-        reply: question.reply,
-        createdAt: new Date(question.createdAt),
-        updatedAt: new Date(question.updatedAt),
-      };
-    });
-
-    return data;
-  }
-
-  await Promise.all([
-    getQuestions().map((question) => {
-      return db.question.create({ data: question });
-    }),
-  ]);
+  await Promise.all(
+    getQuestions().map((data) => {
+      return db.question.create({ data });
+    })
+  );
 }
 
 seed();
